46dabc362785b2845ee007e02b270eb463d44cfd
Ryan Dias <ryanvicd2112@gmail.com>
2025-06-08 11:17:38 -0300

    mudando nome do site

--- PaginaInicial.php
+++ PaginaInicial.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Barra de Navegação - Exemplo</title>
+    <title>Nba Stats</title>
     <link rel="stylesheet" href="navbar.css"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="css/PaginaInicial.css">
 </head>
@@ -29,13 +29,13 @@
                 <ul>
                     <li><a href="#">Jogadores</a></li>
                     <li><a href="CadastrarJogador.php">Cadastrar Jogadores</a></li>
-                    <li><a href="#">Cadastrar Conferencia</a></li>
+                    <li><a href="#">Conferencias</a></li>
                   
                 </ul>
             </nav>
 
             <div class="navbar-actions">
-                <a href="#" class="btn btn-primary">League Pass</a>
+                <a href="https://www.nba.com/league-pass-purchase" class="btn btn-primary">League Pass</a>
                 <a href="https://www.lojanba.com/?gad_source=1&gad_campaignid=938512033&gclid=CjwKCAjwo4rCBhAbEiwAxhJlCay62rU5L7UPk8errIDoaW4-8vJazlma-5oevfFBODys1ASFLmEMphoCfMUQAvD_BwE" class="btn">Loja</a>
                 <a href="https://www.ticketmaster.com.br/event/nbahouse?utm_source=google-ads&utm_medium=cpc&utm_campaign=dojo_2025_nbab_2025_nba_0001_nbahouse_0001_gads_sit_cpc_na_4_na&utm_content=lancamento_cs_gads-gads-sea_sea_lin_pro-sav-seg014-termosgenricoseinstitucionais_geo_mult_18-55_w16apr&utm_term=lancamento_inch_nba-house-2025_na_na_na_sea_dim021_cta017_web_all&gad_source=1&gad_campaignid=22448207946&gclid=CjwKCAjwo4rCBhAbEiwAxhJlCRheIVpJI_N7_voFPLSK2zp9Chbp7U67t2sUisv6B0yP5xSe6NTU3BoCVvMQAvD_BwE" class="btn">Ingressos</a>
                 <a href="#" class="btn"><i class="fas fa-ellipsis-h"></i></a> <a href="#" class="btn"><i class="fas fa-user"></i> Sign in</a>
