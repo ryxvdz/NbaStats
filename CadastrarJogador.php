7f991e19250b315dc6621fe81268f7c968472c64
Ryan Dias <ryanvicd2112@gmail.com>
2025-06-09 16:00:57 -0300

    trocando a estilização do cadastrarJogadores e a fonte

--- CadastrarJogador.php
+++ CadastrarJogador.php
@@ -6,9 +6,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Jogador de Basquete</title>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/CadastrarJogador.css">
 </head>
 <body>
+    <header class="navbar">
+        <div class="navbar-container">
+            <a href="PaginaInicial.php" class="navbar-brand">
+                NBA Stats
+            </a>
+
+            <input type="checkbox" id="menu-toggle" class="menu-toggle">
+            <label for="menu-toggle" class="hamburger">
+                <i class="fas fa-bars"></i>
+            </label>
+
+            <nav class="navbar-nav">
+                <ul>
+                    <li><a href="#">Jogadores</a></li>
+                    <li><a href="CadastrarJogador.php">Cadastrar Jogadores</a></li>
+                    <li><a href="https://www.nba.com/standings">Conferencias</a></li>
+                </ul>
+            </nav>
+
+            <div class="navbar-actions">
+                <a href="https://www.nba.com/league-pass-purchase" class="btn btn-primary">League Pass</a>
+                <a href="https://www.lojanba.com/?gad_source=1&gad_campaignid=938512033&gclid=CjwKCAjwo4rCBhAbEiwAxhJlCay62rU5L7UPk8errIDoaW4-8vJazlma-5oevfFBODys1ASFLmEMphoCfMUQAvD_BwE" class="btn">Loja</a>
+                <a href="https://www.ticketmaster.com.br/event/nbahouse?utm_source=google-ads&utm_medium=cpc&utm_campaign=dojo_2025_nbab_2025_nba_0001_nbahouse_0001_gads_sit_cpc_na_4_na&utm_content=lancamento_cs_gads-gads-sea_sea_lin_pro-sav-seg014-termosgenricoseinstitucionais_geo_mult_18-55_w16apr&utm_term=lancamento_inch_nba-house-2025_na_na_na_sea_dim021_cta017_web_all&gad_source=1&gad_campaignid=22448207946&gclid=CjwKCAjwo4rCBhAbEiwAxhJlCRheIVpJI_N7_voFPLSK2zp9Chbp7U67t2sUisv6B0yP5xSe6NTU3BoCVvMQAvD_BwE" class="btn">Ingressos</a>
+                <a href="#" class="btn"><i class="fas fa-ellipsis-h"></i></a>
+                <a href="#" class="btn"><i class="fas fa-user"></i> Sign in</a>
+            </div>
+        </div>
+    </header>
     <div class="container">
         <h1>Cadastre seu Jogador!</h1>
         <form action="processaJogador.php" method="post">
